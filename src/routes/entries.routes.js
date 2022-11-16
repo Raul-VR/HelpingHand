7a434ed5c5aefb2da5414a0ec6eb4001674fe0ec
Raul V <74110279+Raul-VR@users.noreply.github.com>
2022-11-16 16:50:41 -0600

    Update to login

--- src/routes/entries.routes.js
+++ src/routes/entries.routes.js
@@ -2,23 +2,24 @@ const { Router } = require('express');
 const router = Router();
 
 
-const{ renderIndex, renderNewEntry, createNewEntry, renderLogIn, createLogIn, renderRecived ,renderSignUp, createSignUp, renderChart} = require('../controllers/entries.controllers')
+const{ renderIndex, createIndex, renderNewEntry, createNewEntry, renderLogIn, createLogIn, renderRecived ,renderSignUp, createSignUp, renderChart, createEntries, renderEntries} = require('../controllers/entries.controllers')
 
 router.get('/', renderIndex);
+router.post('/', createIndex);
 
 router.get('/new-entry', renderNewEntry);
-
 router.post('/new-entry', createNewEntry);
 
 router.get('/log-in', renderLogIn);
-
 router.post('/log-in', createLogIn);
 
 router.get('/recibido', renderRecived);
 
 router.get('/sign-up', renderSignUp);
-
 router.post('/sign-up', createSignUp);
+
+router.get('/entries', createEntries);
+router.post('/entries', renderEntries);
 
 router.get('/chart', renderChart);
 
